9a90019a721161861365725a0d9ea6995e408283
SHIVAM MISHRA <140136558+shivam2003mishra@users.noreply.github.com>
2026-01-16 19:09:42 +0530

    [704] [Time Beats: 100.00%] [Memory Beats: 7.67%] - LeetPush

---
+++ DSAinPython/Binary-Search.py
@@ -0,0 +1,17 @@
+1class Solution:
+2    def search(self, nums: List[int], target: int) -> int:
+3        left=0
+4        right=len(nums)-1
+5
+6        while(left <= right):
+7            mid=(left + right)//2
+8
+9            if nums[mid]==target:
+10                return mid
+11            elif nums[mid] < target:
+12                left=mid+1
+13            else:
+14                right=mid-1
+15
+16        return -1
+17        
